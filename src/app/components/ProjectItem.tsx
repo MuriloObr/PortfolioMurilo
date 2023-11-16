88b96844a57142a851fe1c32b1b8c50389e7479c
MuriloObr <muriloobregao@gmail.com>
2023-11-16 12:59:21 -0300

    varias mudancas nos componentes, interface e textos

--- src/app/components/ProjectItem.tsx
+++ src/app/components/ProjectItem.tsx
@@ -1,15 +1,6 @@
-import { ReactNode } from "react"
-import Image from "next/image"
-const technologies = [
-  "git",
-  "reactjs",
-  "nextjs",
-  "tailwind",
-  "javascript",
-  "typescript",
-  "html-5",
-  "css",
-]
+/* eslint-disable @next/next/no-img-element */
+import { ReactNode } from 'react'
+import { TechIconList } from './TechIconList'
 
 interface ProjectItemProps {
   tittle: string
@@ -18,84 +9,49 @@ interface ProjectItemProps {
     mobile: string
   }
   links: string[]
-  tags: typeof technologies
+  tags: string[]
   children: ReactNode
 }
 
 export function ProjectItem({
   tittle,
-  screensImgs: { desktop, mobile },
+  screensImgs: { desktop },
   links,
   tags,
   children,
 }: ProjectItemProps) {
   return (
-    <div
-      className={`w-5/6 py-5 px-12 flex flex-col items-center gap-5 border-l-2 border-primary-dark relative z-10 hover:after:w-full group/project max-sm:w-full max-sm:px-8
-                after:absolute after:inset-0 after:-z-10 after:w-0 after:h-full after:bg-gradient-to-r after:from-primary after:to-accent after:transition-all after:duration-500 after:delay-100`}
-    >
-      <h2 className="self-start text-2xl text-defaultBlack group-hover/project:text-white">
+    <div className="group/project relative z-10 flex w-5/6 flex-col items-center gap-5 px-12 py-5 text-defaultBlack max-sm:w-full max-sm:px-8">
+      <h2 className="self-start rounded-md bg-gradient-to-r from-accent/70 to-primary/70 px-4 py-2 text-3xl text-white">
         {tittle}
       </h2>
-      <div className="relative self-start">
-        <Image
-          src="/desktop-screen.png"
-          alt="Desktop Screen"
-          height={600}
-          width={600}
-          className="relative z-10"
-        />
-        <Image
-          src={desktop}
-          alt="Desktop Version"
-          height={600}
-          width={585}
-          className="absolute top-6 left-3 max-sm:top-3"
-        />
-        <Image
-          src="/mobile-screen.png"
-          alt="Mobile Screen"
-          height={120}
-          width={118}
-          className="absolute bottom-0 -right-5 z-20"
-        />
-        <Image
-          src={mobile}
-          alt="Mobile Version"
-          height={100}
-          width={109}
-          className="absolute -bottom-0.5 -right-3.5 z-10 rounded-lg max-sm:-right-3 max-sm:w-[6.6rem] max-sm:bottom-1"
-        />
+      <div className="flex gap-5 overflow-hidden rounded-md">
+        <div className="w-4/5">
+          <img
+            src={desktop}
+            alt="Desktop Version"
+            height={500}
+            width={500}
+            className="w-full"
+          />
+        </div>
+        <p className="w-2/3 text-justify text-xl">
+          {children}
+          <ul className="mt-5 w-1/2 self-start">
+            <TechIconList list={tags} hoverEffect />
+          </ul>
+        </p>
       </div>
-      <p className="text-justify select-none max-sm:text-defaultBlack">
-        {children}
-      </p>
-      <ul className="self-start w-1/4 flex justify-around max-sm:w-2/4">
-        {tags.map((tag) => (
-          <li
-            key={tag}
-            className="saturate-0 opacity-80 hover:saturate-100 hover:opacity-100 max-sm:saturate-100 max-sm:opacity-100"
-          >
-            <Image
-              src={`/${tag}.svg`}
-              alt="tech tag"
-              height={35}
-              width={35}
-              className="w-full"
-            />
-          </li>
-        ))}
-      </ul>
-      <div className="w-1/2 flex justify-between text-xl font-bold">
+      <div className="flex w-1/2 justify-between text-xl font-bold">
         <a
           href={links[0]}
-          className="relative after:block after:absolute after:left-0 after:w-0 after:hover:w-full after:transition-all transition-all after:h-0.5 after:bg-white after:duration-300"
+          className="relative transition-all after:absolute after:left-0 after:block after:h-0.5 after:w-0 after:bg-defaultBlack after:transition-all after:duration-300 after:hover:w-full"
         >
           Repositório
         </a>
         <a
           href={links[1]}
-          className="relative after:block after:absolute after:left-0 after:w-0 after:hover:w-full after:transition-all transition-all after:h-0.5 after:bg-white after:duration-300"
+          className="relative transition-all after:absolute after:left-0 after:block after:h-0.5 after:w-0 after:bg-defaultBlack after:transition-all after:duration-300 after:hover:w-full"
         >
           ↪ Visitar
         </a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,217 +1,116 @@
-import Image from "next/image"
-import { ProjectItem } from "./components/ProjectItem"
+import Image from 'next/image'
+import { ProjectItem } from './components/ProjectItem'
+import { TechIconList } from './components/TechIconList'
 
 const technologies = [
-  "html-5",
-  "css",
-  "javascript",
-  "typescript",
-  "git",
-  "nextjs",
-  "reactjs",
-  "tailwind",
-  "python",
+  'html-5',
+  'css',
+  'javascript',
+  'typescript',
+  'git',
+  'docker',
+  'nextjs',
+  'reactjs',
+  'tailwind',
+  'python',
 ]
 
 export default function Home() {
   return (
-    <>
-      <header
-        className="h-screen-d w-full bg-gradient-to-tr from-primary/80 to-accent/80 p-5"
-        id="start"
+    <main className="flex h-fit w-full flex-col justify-between">
+      <section
+        className="flex h-fit w-full flex-col bg-defaultBG"
+        id="projetos"
       >
-        <div className="flex justify-between px-5 max-sm:p-0 max-sm:flex-wrap">
-          <h1 className="text-2xl font-bold select-none max-sm:w-fit max-sm:mx-auto">
-            {"<Murilo/>"}
-          </h1>
-          <nav className="flex gap-10 text-xl max-sm:gap-5 max-sm:mx-auto">
-            <a
-              href="#projetos"
-              className="text-slate-400 hover:text-white relative after:block after:absolute after:left-0 after:w-0 after:hover:w-full after:transition-all transition-all after:h-0.5 after:bg-white"
-            >
-              Projetos
-            </a>
-            <a
-              href="#sobre"
-              className="text-slate-400 hover:text-white relative after:block after:absolute after:left-0 after:w-0 after:hover:w-full after:transition-all transition-all after:h-0.5 after:bg-white"
-            >
-              Sobre Mim
-            </a>
-            <a
-              href="#contato"
-              className="text-slate-400 hover:text-white relative after:block after:absolute after:left-0 after:w-0 after:hover:w-full after:transition-all transition-all after:h-0.5 after:bg-white"
-            >
-              Contato
-            </a>
-          </nav>
-        </div>
-        <div className="h-fit w-fit absolute inset-0 my-auto mx-auto flex flex-col z-10 select-none max-sm:ml-8">
-          <span className="text-6xl text-transparent bg-clip-text bg-gradient-to-tr from-defaultBlack to-primary font-extrabold tracking-tight uppercase">
-            FullStack Developer
-          </span>
-          <span className="text-7xl font-bold antialiased tracking-tight">
-            Murilo Dressel Obregão
-          </span>
-        </div>
-      </header>
-      <div className="bg-white">
-        <main className="h-fit w-full flex flex-col justify-between">
-          <section
-            className="h-fit w-full bg-defaultBG flex flex-col"
-            id="projetos"
+        <h2 className="ml-5 mt-5 select-none text-4xl font-bold text-defaultBlack">
+          Projetos
+        </h2>
+        <div className="flex flex-col justify-items-center gap-36 px-8 py-10">
+          <ProjectItem
+            tittle="ForUm"
+            screensImgs={{
+              desktop: '/forum-desktop.png',
+              mobile: '/mobile',
+            }}
+            tags={['typescript', 'reactjs', 'tailwind', 'python', 'docker']}
+            links={[
+              'https://github.com/MuriloObr/ForUm_Front-End',
+              'https://for-um-front-end.vercel.app/',
+            ]}
           >
-            <h2 className="text-4xl text-defaultBlack ml-5 mt-5 select-none font-bold">
-              Projetos
-            </h2>
-            <div className="py-10 grid grid-cols-2 justify-items-center gap-36 max-sm:grid-cols-1">
-              <ProjectItem
-                tittle="ForUm"
-                screensImgs={{
-                  desktop: "/forum-desktop.png",
-                  mobile: "/mobile",
-                }}
-                tags={["typescript", "reactjs", "tailwind", "python"]}
-                links={[
-                  "https://github.com/MuriloObr/ForUm_Front-End",
-                  "https://for-um-front-end.vercel.app/",
-                ]}
-              >
-                Criei um fórum em que o usuário pode fazer login, postar,
-                comentar e dar e remover likes, utilizando várias libs, além de
-                utilizar python com flask no backend. Neste Projeto aprendi
-                melhor sobre banco de dados, ORM (usei sqlalchemy) e APIs.
-              </ProjectItem>
-              <ProjectItem
-                tittle="Twitter Clone"
-                screensImgs={{
-                  desktop: "/twitter-desktop.jpg",
-                  mobile: "/twitter-mobile.jpg",
-                }}
-                tags={["nextjs", "reactjs", "tailwind", "typescript"]}
-                links={[
-                  "https://github.com/MuriloObr/TwitterClone",
-                  "https://twitter-clone-six-lemon.vercel.app",
-                ]}
-              >
-                Criei um clone da interface do twitter, utilizando next com
-                tailwind. Nesse projeto aprendi um pouco sobre ContextApi e
-                algumas formas melhores de escrever o react, além de ser o
-                primeiro projeto que usei typescript.
-              </ProjectItem>
-              <ProjectItem
-                tittle="To-Do List"
-                screensImgs={{
-                  desktop: "/todo-list-desktop.jpg",
-                  mobile: "/todo-list-mobile.jpg",
-                }}
-                tags={["javascript", "html-5", "css"]}
-                links={[
-                  "https://github.com/MuriloObr/Lista-de-Atividades-To-Do-com-JS-vanilla",
-                  "https://muriloobr.github.io/Lista-de-Atividades-To-Do-com-JS-vanilla/",
-                ]}
-              >
-                Criei uma to-do list em que o usuário pode salvar presets de
-                atividades, usei o localStorage para armazenar os dados e
-                treinei minha capacidade em criar código reutilizável.
-              </ProjectItem>
-            </div>
-          </section>
-          <section
-            className="h-fit w-full flex flex-col items-center bg-secondary p-5"
-            id="sobre"
+            Criei um fórum em que o usuário pode fazer login, postar, comentar e
+            dar e remover likes, utilizando várias libs, além de utilizar python
+            com flask no backend. Neste Projeto aprendi melhor sobre banco de
+            dados, ORM (usei sqlalchemy) e APIs. Recentemente mudei o backend
+            para um container docker para poder utilizar a ultima versão do
+            Flask.
+          </ProjectItem>
+          <ProjectItem
+            tittle="Twitter Clone"
+            screensImgs={{
+              desktop: '/twitter-desktop.jpg',
+              mobile: '/twitter-mobile.jpg',
+            }}
+            tags={['nextjs', 'reactjs', 'tailwind', 'typescript']}
+            links={[
+              'https://github.com/MuriloObr/TwitterClone',
+              'https://twitter-clone-six-lemon.vercel.app',
+            ]}
           >
-            <h2 className="text-4xl self-start select-none font-bold text-defaultBlack">
-              Sobre
-            </h2>
-            <div className="w-3/4 mx-auto text-defaultBlack max-sm:w-11/12">
-              <div className="flex justify-center items-center gap-10 max-sm:flex-col">
-                <div className="border-2 border-secondary bg-secondary h-fit w-fit rounded-3xl overflow-hidden relative group/myImg">
-                  <h3 className="h-fit w-fit text-5xl tracking-tighter leading-tight absolute inset-0 m-auto text-defaultBlack select-none group-hover/myImg:-z-20 transition-all duration-500 max-sm:my-5">
-                    Murilo <br /> Dressel <br /> Obregão
-                  </h3>
-                  <Image
-                    src="/minha foto sem fundo.png"
-                    alt="Minha Foto"
-                    width={200}
-                    height={200}
-                    className="opacity-0 group-hover/myImg:opacity-100 transition-all duration-500 max-sm:h-52"
-                  />
-                </div>
-                <p className="w-3/6 text-justify text-lg max-sm:w-5/6">
-                  Olá meu nome é Murilo, eu comecei na programação no 9 ano do
-                  fundamental com python no curso em vídeo do mestre Gustavo
-                  Guanabara, criei alguns projetos simples e depois fiquei muito
-                  tempo sem praticar. Há mais ou menos 1 ano e meio voltei com a
-                  intenção de seguir com web, aprendi HTML, CSS e JS e
-                  atualmente utilizo bastante o Next e estou aprendendo Ruby
-                  para criar o back-end com rails.
-                </p>
-              </div>
-              <h3 className="w-fit mx-auto text-xl mb-2 tracking-widest max-sm:mt-8">
-                Tecnologias que eu utilizo
-              </h3>
-              <ul className="w-fit mx-auto mb-5 flex gap-4">
-                {technologies.map((tech) => (
-                  <li key={tech}>
-                    <Image
-                      alt={`${tech} Image`}
-                      src={`/${tech}.svg`}
-                      height={50}
-                      width={50}
-                    />
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </section>
-        </main>
-        <footer id="contato" className="bg-white p-10">
-          <div className="h-fit w-fit p-8 m-auto bg-secondary rounded-lg">
-            <h2 className="text-defaultBlack text-2xl font-bold mx-auto w-fit">
-              Entre em contato
-            </h2>
-            <div className="h-16 w-fit my-2 mx-auto flex items-center justify-center gap-10">
-              <a
-                href="https://github.com/MuriloObr"
-                className="bg-white rounded-xl p-2 invert transition-all hover:mb-2 hover:shadow-lg hover:shadow-white"
-              >
-                <Image
-                  src="/githubIcon.svg"
-                  height={35}
-                  width={35}
-                  alt="media icon"
-                />
-              </a>
-              <a
-                href="https://linkedin.com/in/murilo-obregão/"
-                className="bg-blue-600 rounded-xl p-2 transition-all hover:mb-2 hover:shadow-lg hover:shadow-blue-500"
-              >
-                <Image
-                  src="/linkedin.svg"
-                  height={35}
-                  width={35}
-                  alt="media icon"
-                  className="invert"
-                />
-              </a>
-              <a className="bg-white rounded-xl p-1 cursor-pointer transition-all hover:mb-2 hover:shadow-lg hover:shadow-red-500">
-                <Image
-                  src="/gmail.svg"
-                  height={45}
-                  width={45}
-                  alt="media icon"
-                />
-              </a>
-            </div>
+            Criei um clone da interface do twitter, utilizando next com
+            tailwind. Nesse projeto aprendi um pouco sobre ContextApi e algumas
+            formas melhores de escrever o react, além de ser o primeiro projeto
+            que usei typescript.
+          </ProjectItem>
+          <ProjectItem
+            tittle="To-Do List"
+            screensImgs={{
+              desktop: '/todo-list-desktop.jpg',
+              mobile: '/todo-list-mobile.jpg',
+            }}
+            tags={['javascript', 'html-5', 'css']}
+            links={[
+              'https://github.com/MuriloObr/Lista-de-Atividades-To-Do-com-JS-vanilla',
+              'https://muriloobr.github.io/Lista-de-Atividades-To-Do-com-JS-vanilla/',
+            ]}
+          >
+            Criei uma to-do list em que o usuário pode salvar presets de
+            atividades, usei o localStorage para armazenar os dados e treinei
+            minha capacidade em criar código reutilizável.
+          </ProjectItem>
+        </div>
+      </section>
+      <section
+        className="flex h-fit w-full flex-col items-center bg-secondary p-5"
+        id="sobre"
+      >
+        <h2 className="select-none self-start text-4xl font-bold text-defaultBlack">
+          Sobre
+        </h2>
+        <div className="mx-auto w-3/4 text-defaultBlack max-sm:w-11/12">
+          <div className="flex items-center justify-center gap-10 max-sm:flex-col">
+            <h3 className="mx-3 h-fit w-fit select-none text-5xl leading-tight tracking-tighter text-defaultBlack transition-all duration-500 max-sm:my-5">
+              Murilo <br /> Dressel <br /> Obregão
+            </h3>
+            <p className="mb-5 w-3/6 text-justify text-xl max-sm:w-5/6">
+              Olá meu nome é Murilo, eu comecei na programação no 9 ano do
+              fundamental com python no curso em vídeo do mestre Gustavo
+              Guanabara, criei alguns projetos simples e depois fiquei muito
+              tempo sem praticar. Há 2 anos voltei a aprender e com isso entrei
+              no mundo da web com html, css e js logo depois aprendi react e
+              nextjs e utilizei meu conhecimento em python para fazer o backend.
+              Atualmente estou em busca de aprender a utilizar prismaJS e nestJS
+              para utilizar o próprio typescript no backend.
+            </p>
           </div>
-        </footer>
-        <a
-          href="#start"
-          className="p-3 sticky bottom-3 left-full mr-3 rounded-md bg-primary z-50"
-        >
-          Voltar ao topo ↑
-        </a>
-      </div>
-    </>
+          <h3 className="mx-auto mb-2 w-fit text-xl tracking-widest max-sm:mt-8">
+            Tecnologias que eu utilizo
+          </h3>
+          <ul className="mx-auto mb-5 flex w-2/3 gap-4">
+            <TechIconList list={technologies} />
+          </ul>
+        </div>
+      </section>
+    </main>
   )
 }
